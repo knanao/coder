371ceb9bb2e27211457cc98e21c8e2c5f517c9d8
knanao <nao.7ken@gmail.com>
2021-05-22 12:58:49 +0900

    fixup

--- algorithm/617_merge_two_trees.py
+++ algorithm/617_merge_two_trees.py
@@ -1,5 +1,3 @@
-# Definition for a binary tree node.
-
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -13,7 +11,7 @@ class Solution:
             return root2
         if not root2:
             return root1
-        t = TreeNode(root1.val+root2.val)
+        t = TreeNode(root1.val + root2.val)
         t.left = self.mergeTrees(root1.left, root2.left)
         t.right = self.mergeTrees(root1.right, root2.right)
         return t
